282934f7c6fa906b0929306aae643a77ed12ab92
Kebo Pabrireols <viet4112006@gmail.com>
2026-03-24 21:41:00 +0700

    user

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,4 +1,28 @@
 package services;
 
+import dao.UserDAO;
+import models.User;
+import java.sql.Connection;
+import java.util.List;
+
 public class UserService {
+    private UserDAO userDAO;
+    public UserService(Connection conn) {
+        userDAO = new UserDAO(conn);
+    }
+    // lấy danh sách user
+    public List<User> getAllUsers() {
+        // chỉ gọi xuống DAO để lấy dữ liệu
+        return userDAO.findAll();
+    }
+    // thêm user
+    public boolean addUser(User user) {
+        // kiểm tra dữ liệu trước khi cho xuống DB
+        if (user.getFullName() == null || user.getFullName().trim().isEmpty()) {
+            System.out.println("Tên không được để trống");
+            return false;
+        }
+        // hợp lệ thì gọi DAO để insert
+        return userDAO.insert(user);
+    }
 }
