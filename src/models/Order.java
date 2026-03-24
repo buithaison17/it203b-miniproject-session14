04a2af4f05fe442be087f62b1abe328d331062ec
sonbui <thaisonb936@gmail.com>
2026-03-24 21:11:43 +0700

    up code

--- src/models/Order.java
+++ src/models/Order.java
@@ -1,4 +1,49 @@
 package models;
 
+import java.time.LocalDate;
+
 public class Order {
+    private int orderId;
+    private int userId;
+    private LocalDate orderDate;
+    private double totalAmount;
+
+    public Order(int orderId, int userId, LocalDate orderDate, double totalAmount) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
 }
